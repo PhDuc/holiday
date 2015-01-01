9306ebde44c2968a3e4e8e0dbae27ce4b55477c6
Duc Phan <phanducsjsu@gmail.com>
2015-01-01 09:43:53 -0800

    Railsify ittt

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Holiday::Application.config.secret_token = '4fb523978e1f3ad96e04a8857829e4db6708766e8ca604300a895bfaf5681c91a36572290b5b90964686489bbfa23debd25f893a96ed42389e6b9b0d795be507'
